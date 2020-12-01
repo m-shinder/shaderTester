8ebdb4208cf121b63383904eb550956c3bc749a8
Mihail <mihail@shinder.ml>
2020-12-01 17:02:02 +0200

    Stuck with errors

--- src/glarea-functions.c
+++ src/glarea-functions.c
@@ -7,6 +7,51 @@
 
 int setVertices(RenderOptions* options, RenderData* data)
 {
+	float vertices[] = {
+	//	  X		 Y		Z		 R	   G	 B	   A		 S	   T
+		-0.5f, -0.5f,  0.0f,	1.0f, 1.0f, 0.0f, 1.0f,		0.0f, 0.0f,
+		-0.5f,  0.5f,  0.0f,	1.0f, 0.0f, 1.0f, 1.0f,		0.0f, 1.0f,
+		 0.5f,  0.5f,  0.0f,	0.0f, 1.0f, 1.0f, 1.0f,		1.0f, 1.0f,
+		 0.5f, -0.5f,  0.0f,	1.0f, 1.0f, 1.0f, 1.0f,		1.0f, 0.0f
+	};
+	unsigned int indices[] = {
+		0, 1, 2,
+		2, 3, 0
+	};
+	printf("glerrV %d\n",glGetError());
+	glGenVertexArrays(1, &(data->VAO));
+	glBindVertexArray(data->VAO);
+
+	glGenBuffers(1,&(data->VBO));
+	glBindBuffer(GL_ARRAY_BUFFER, data->VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glGenBuffers(1, &(data->EBO));
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, data->EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 9 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 9 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 9 * sizeof(float), (void*)(7 * sizeof(float)));
+	glEnableVertexAttribArray(2);
+
+	// TODO: make it depends on options
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	glUniform1i(glGetUniformLocation(data->shaderProgram,"Texture1"), 0);
+	glUniform1i(glGetUniformLocation(data->shaderProgram,"Texture2"), 1);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+	printf("glerrVE %d\n",glGetError());
 
 }
 
@@ -73,6 +118,30 @@ gboolean on_glarea_render(GtkGLArea* area, GdkGLContext* context, RenderData* re
 {
 	glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT);
+	printf("glerr0 %d\n",glGetError());
+
+	glUseProgram(renderData->shaderProgram);
+	//printf("glerrM %d\n",glGetError());
+	//glUniform1f(glGetUniformLocation(renderData->shaderProgram, "xOffset"), 0.0f);
+	//printf("glerrm %d\n",glGetError());
+	glActiveTexture(GL_TEXTURE0);
+	printf("glerr1 %d\n",glGetError());
+	glBindTexture(GL_TEXTURE_2D, renderData->firstTexture);
+	printf("glerr2 %d\n",glGetError());
+	glActiveTexture(GL_TEXTURE1);
+	glBindTexture(GL_TEXTURE_2D, renderData->secondTexture);
+	printf("glerr3 %d\n",glGetError());
+	glBindVertexArray(renderData->VAO);
+	printf("glerr4 %d\n",glGetError());
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, renderData->EBO);
+	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+	glBindVertexArray(0);
+	glUseProgram(0);
+	glFlush();
+
+	printf("glerr5 %d\n",glGetError());
+	g_print("render flushed!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!111\n");
+
 	return FALSE;
 }
 
